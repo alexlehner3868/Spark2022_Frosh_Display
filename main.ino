2dccf73e0db14846465b0dea61b7975d8c1551fd
elizabethtyra <90435326+elizabethtyra@users.noreply.github.com>
2022-07-22 06:34:59 -0400

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,3 +1,13 @@
+#include <FastLED.h>
+
+#define NUM_LEDS 32
+
+CRGB leds[NUM_LEDS];
+
+// define variables used by the sequences
+uint8_t hue = 0;                                       // starting hue
+
+
 void setup() {
   // put your setup code here, to run once:
  
@@ -5,5 +15,18 @@ void setup() {
  
 void loop() {
   // put your main code here, to run repeatedly:
- 
+  
+  
+  // 1. colour change 
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CHSV(hue, 255, 255);
+  }
+  EVERY_N_MILLISECONDS(100){
+    hue++;
+  }
+
+  
+  FastLED.show();
+  
+  
 }
